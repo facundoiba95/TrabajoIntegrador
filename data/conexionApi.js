bf79da3829d5139d4b880d261fb92251bc08323e
facundoiba95 <facundoiba95@gmail.com>
2022-10-11 21:04:20 -0300

    modifique conexionAPi.js

--- data/conexionApi.js
+++ data/conexionApi.js
@@ -2,10 +2,8 @@ const urlBase = `https://api-eshopcba.herokuapp.com/api`;
 
 const requestApi = async ()=> {
     try {
-        const queryParam = ``;
-        const resultado = await fetch(urlBase + queryParam);
+        const resultado = await fetch(urlBase);
         const json = await resultado.json();
-      console.log(json)
         return json;
     } catch (error) {
         console.log(error)
